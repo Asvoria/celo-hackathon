eec80dbc3b381aa8dd25bba6cfd2e22469e0de54
ThriftyOldStudent <asvoria@live.com>
2021-04-06 23:00:47 +0800

    error

--- pine_v0.js
+++ pine_v0.js
@@ -43,6 +43,46 @@ async function getName(instance){
     console.log(name)
 }
 
+
+//Selling CELO only if the rate is favorable
+// This is at lower price I will accept in cUSD for every CELO
+const favorableAmount = 100
+const amountToExchange = kit.web3.utils.toWei('10', 'ether')
+const oneGold = kit.web3.utils.toWei('1', 'ether')
+const exchange = await kit.contracts.getExchange()
+
+const amountOfcUsd = await exchange.quoteGoldSell(oneGold)
+
+if (amountOfcUsd > favorableAmount) {
+  const goldToken = await kit.contracts.getGoldToken()
+  const approveTx = await goldToken.approve(exchange.address, amountToExchange).send()
+  const approveReceipt = await approveTx.waitReceipt()
+
+  const usdAmount = await exchange.quoteGoldSell(amountToExchange)
+  const sellTx = await exchange.sellGold(amountToExchange, usdAmount).send()
+  const sellReceipt = await sellTx.waitReceipt()
+}
+
+const goldtoken = await kit._web3Contracts.getGoldToken()
+const oneGold = kit.web3.utils.toWei('1', 'ether')
+
+const txo = await goldtoken.methods.transfer(someAddress, oneGold)
+const tx = await kit.sendTransactionObject(txo, { from: myAddress })
+const hash = await tx.getHash()
+const receipt = await tx.waitReceipt()
+
+const stableToken = await this.contracts.getStableToken()
+const exchange = await this.contracts.getExchange()
+
+const cUsdBalance = await stableToken.balanceOf(myAddress)
+
+const approveTx = await stableToken.approve(exchange.address, cUsdBalance).send()
+const approveReceipt = await approveTx.waitReceipt()
+
+const goldAmount = await exchange.quoteUsdSell(cUsdBalance)
+const sellTx = await exchange.sellDollar(cUsdBalance, goldAmount).send()
+const sellReceipt = await sellTx.waitReceipt()
+
 /*
 // Set the 'name' stored in the HelloWorld.sol contract
 async function setName(instance, newName){
@@ -51,6 +91,9 @@ async function setName(instance, newName){
     // Add your account to ContractKit to sign transactions
     // This account must have a CELO balance to pay tx fees, get some https://celo.org/build/faucet
     kit.connection.addAccount(account.privateKey)
+
+    const amountToExchange = kit.web3.utils.toWei('10', 'ether')
+    const oneGold = kit.web3.utils.toWei('1', 'ether')
     
     // Encode the transaction to HelloWorld.sol according to the ABI
     let txObject = await instance.methods.setName(newName)
